47e4b6a2f258c7d69819eee679926c074b33da46
Freddy Limpens <freddy.limpens@mnemotix.com>
2025-04-09 11:15:19 +0200

    files refacto

--- src/main/groovy/benchmark.groovy
+++ src/main/groovy/benchmark.groovy
@@ -3,8 +3,8 @@ import java.io.File
 // Main execution
 try {
     //def benchmark = new RDFBenchmark("corese.4.6.3")
-    //def benchmark = new RDFBenchmark("rdf4j.5.1.2")
-    def benchmark = new RDFBenchmark("jena.4.10.0")
+    def benchmark = new RDFBenchmark("rdf4j.5.1.2")
+    //def benchmark = new RDFBenchmark("jena.4.10.0")
     //benchmark.processDirectory('/Users/freddylimpens/src/tmp/bowlogna_benchmark/sample')
     benchmark.processDirectory('/Users/freddylimpens/src/tmp/bowlogna_benchmark/BowlognaOutput')
 } catch (Exception e) {
